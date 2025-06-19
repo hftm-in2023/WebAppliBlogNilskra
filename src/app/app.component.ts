455ff14060954b1d8ee5d09b8646e147b60c9133
Nils Krapl <nils.krapl@gmail.com>
2025-06-19 17:13:48 +0200

    style(app): use flexbox layout for blog cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlogService } from './services/blog.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule],
+  template: `
+    <h1>BlogFrog Übersicht</h1>
+
+    @if (blogService.loading()) {
+      <p>Lade Daten...</p>
+    } @else {
+      <div class="blog-container">
+        @for (blog of blogs; track blog.id) {
+          <article class="blog-entry">
+            <img
+              [src]="
+                blog.headerImageUrl && blog.headerImageUrl !== 'string'
+                  ? blog.headerImageUrl
+                  : 'assets/images/platzhalter.png'
+              "
+              alt="Header Image"
+            />
+            <h2>{{ blog.title }}</h2>
+            <p>{{ blog.contentPreview }}</p>
+          </article>
+        } @empty {
+          <p>Keine Blogeinträge vorhanden.</p>
+        }
+      </div>
+    }
+  `,
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'blog-frog';
+export class AppComponent implements OnInit {
+  blogService = inject(BlogService);
+  title = 'angular-blogify-tobias-ragosa';
+
+  ngOnInit() {
+    this.blogService.loadBlogs();
+  }
+
+  get blogs() {
+    return this.blogService.blogEntries();
+  }
+
+  get isLoading() {
+    return this.blogService.loading();
+  }
 }
