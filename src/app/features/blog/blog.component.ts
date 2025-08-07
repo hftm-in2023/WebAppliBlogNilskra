94c2e313321a22c8dd834b7ae4ec6e4820d401a0
Nils Krapl <nils.krapl@gmail.com>
2025-08-07 16:29:20 +0200

    ferien anpassungen

--- src/app/features/blog/blog.component.ts
+++ src/app/features/blog/blog.component.ts
@@ -1,17 +1,18 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { BlogService } from '../../core/services/blog.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss',
 })
 export class BlogComponent implements OnInit {
   blogService = inject(BlogService);
-  title = 'angular-blogify-tobias-ragosa';
+  title = 'nils.krapl';
 
   ngOnInit() {
     this.blogService.loadBlogs();
